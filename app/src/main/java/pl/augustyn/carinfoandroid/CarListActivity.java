42bfe98ee746bc4293f52eca10ff219b7c4703bb
Patryk Augustyn <patryk.augustyn@gmail.com>
2016-01-16 12:19:11 +0100

    Inicjalizacja projektu

---
+++ app/src/main/java/pl/augustyn/carinfoandroid/CarListActivity.java
@@ -0,0 +1,13 @@
+package pl.augustyn.carinfoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_list);
+    }
+}
